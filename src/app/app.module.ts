8d185066be5f732670ca7c53d585b97bac93c06f
ashishmishra95 <mishraashish034@gmail.com>
2018-09-04 03:25:17 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { IngredientService } from './ingredients/ingredient.service';
+import { AppRoutingModule } from './app.routing.module';
 
 
 
@@ -27,6 +28,7 @@ import { IngredientService } from './ingredients/ingredient.service';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   
   ],
   providers: [IngredientService],
